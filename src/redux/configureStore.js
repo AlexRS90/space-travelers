481b18828b25a1f28c55a0bb31867d81331fcaa8
AlexRS90 <aramosbengy@gmail.com>
2021-12-14 14:50:00 -0600

    Add rocket reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,9 +3,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import missionReducer from './mission/mission';
+import rocketReducer from './rocket/rocket';// eslint-disable-line
 
 const reducer = combineReducers({
   missions: missionReducer,
+  rockets: rocketReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(logger, thunk));
