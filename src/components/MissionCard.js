a93af80b726cfe6c9dcb79c0c3b0343c99e2a36c
micheaol <micheaol80@gmail.com>
2021-12-14 16:36:53 +0100

    display mission, list render

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './MissionCard.css';
 
-const MissionCard = () => (
+const MissionCard = ({ mission }) => (
   <>
-    <h1>Hello card</h1>
+    <tbody>
+      <tr>
+        <td>{mission.mission_name}</td>
+        <td className="mission-desc">{mission.description}</td>
+        <td className="mission-action">
+          <span className="me-3">Active Member</span>
+          <span>Join Mission</span>
+        </td>
+      </tr>
+    </tbody>
   </>
 );
+
+MissionCard.propTypes = {
+  mission: PropTypes.node.isRequired,
+};
 
 export default MissionCard;
