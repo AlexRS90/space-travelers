6cc14ff069f33cf3a44dfd1d1cfa08580189d849
micheaol <micheaol80@gmail.com>
2021-12-14 11:33:16 +0100

    add logic to fetch missons

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,17 @@
-const Mission = () => (
-  <p>This is missions section</p>
-);
+/* eslint-disable import/extensions */
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMission } from '../redux/mission/mission';
+
+const Mission = () => {
+  const dispatch = useDispatch();
+  const mission = useSelector((state) => state.mission);
+
+  useEffect(() => {
+    dispatch(fetchMission());
+  }, []);
+  console.log(mission);
+  return <p>This is missions section</p>;
+};
 
 export default Mission;
