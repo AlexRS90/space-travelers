2b362f69ed8cf2e6dd6d170a5b6b6b9bdbb12a19
AlexRS90 <aramosbengy@gmail.com>
2021-12-14 14:51:21 -0600

    Fetch API data

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,5 +1,22 @@
-const Rockets = () => (
-  <p>This is Rockets section</p>
-);
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRocket } from '../redux/rocket/rocket';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rocket = useSelector((state) => state.rockets.rockets);
+  useEffect(() => {
+    dispatch(fetchRocket());
+  }, []);
+  return (
+    <div>
+      {rocket.map((cohete) => (
+        <div key={cohete[0].id}>
+          <h1>{cohete[0].name}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Rockets;
