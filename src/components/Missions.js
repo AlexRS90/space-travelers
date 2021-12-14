1c4800bc4849c4903daaf2bbd7df61f0de9b0fc5
micheaol <micheaol80@gmail.com>
2021-12-14 12:29:59 +0100

    add card to the mission component

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,18 +1,27 @@
-/* eslint-disable import/extensions */
+/* eslint-disable max-len */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import MissionCard from './MissionCard';
 import { fetchMission } from '../redux/mission/mission';
+import './Missions.css';
 
 const Mission = () => {
   const dispatch = useDispatch();
   const mission = useSelector((state) => state.missions);
-  console.log('Components mission', mission);
 
   useEffect(() => {
     dispatch(fetchMission());
   }, []);
-  return <MissionCard />;
+  return (
+    <>
+      <div className="mission-header">
+        <h1>Mission</h1>
+        <h1>Description</h1>
+        <h1>Status</h1>
+      </div>
+      {mission && mission.map((mission) => <MissionCard mission={mission} key={mission.mission_id} />)}
+    </>
+  );
 };
 
 export default Mission;
