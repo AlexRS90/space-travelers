b61188cd0ea9c3d1aae731dd8513e3a0403cdb43
AlexRS90 <aramosbengy@gmail.com>
2021-12-14 23:09:12 -0600

    Render fetch data

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,18 +1,26 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchRocket } from '../redux/rocket/rocket';
+import Button from './bookRocket';
+import './Rockets.css';
+/*eslint-disable*/
 
 const Rockets = () => {
   const dispatch = useDispatch();
-  const rocket = useSelector((state) => state.rockets.rockets);
+  const rocket = useSelector((state) => state.rockets);
   useEffect(() => {
     dispatch(fetchRocket());
   }, []);
   return (
-    <div>
+    <div className="container34">
       {rocket.map((cohete) => (
-        <div key={cohete[0].id}>
-          <h1>{cohete[0].name}</h1>
+        <div key={cohete.id} className="wrapper">
+          <img src={cohete.image} alt={`Rocket: ${cohete.name}`} width="300" />
+          <div className="info-wrapper">
+            <h2>{cohete.name}</h2>
+            <p>{cohete.description}</p>
+            <Button id={cohete.id}/>
+          </div>
         </div>
       ))}
     </div>
