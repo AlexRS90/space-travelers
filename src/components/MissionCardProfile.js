da36d8a8825da5157b2db436ead07964210755da
micheaol <micheaol80@gmail.com>
2021-12-15 11:13:44 +0100

    add logic to render joined mission in the profile component

---
+++ src/components/MissionCardProfile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MissionCardProfile = () => (
+  <div>
+    Hello from card
+  </div>
+);
+
+export default MissionCardProfile;
--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,17 @@
-const MyProfile = () => (
-  <p>This is my profile section</p>
-);
+/* eslint-disable max-len */
+import React, { } from 'react';
+import { useSelector } from 'react-redux';
+import MissionCardProfile from './MissionCardProfile';
+
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missions);
+  const reservedMissions = missions.filter((mission) => mission.reserved === true);
+
+  return (
+    <>
+      {reservedMissions && reservedMissions.map((reserMisson) => <MissionCardProfile reserMisson={reserMisson} key={reserMisson.mission_id} />)}
+    </>
+  );
+};
 
 export default MyProfile;
