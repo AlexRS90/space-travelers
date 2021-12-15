a40b304940a7cc782ddb362a9093722e1b76e19c
micheaol <micheaol80@gmail.com>
2021-12-15 11:28:50 +0100

    display joined missions in my profile component

--- src/components/MissionCardProfile.js
+++ src/components/MissionCardProfile.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const MissionCardProfile = () => (
-  <div>
-    Hello from card
-  </div>
+const MissionCardProfile = ({ reserMisson }) => (
+  <tbody>
+    <tr>
+      <td>{reserMisson.mission_name}</td>
+    </tr>
+  </tbody>
 );
+
+MissionCardProfile.propTypes = {
+  reserMisson: PropTypes.node.isRequired,
+};
 
 export default MissionCardProfile;
--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,15 +2,23 @@
 import React, { } from 'react';
 import { useSelector } from 'react-redux';
 import MissionCardProfile from './MissionCardProfile';
+import './MyProfile.css';
 
 const MyProfile = () => {
   const missions = useSelector((state) => state.missions);
   const reservedMissions = missions.filter((mission) => mission.reserved === true);
 
   return (
-    <>
-      {reservedMissions && reservedMissions.map((reserMisson) => <MissionCardProfile reserMisson={reserMisson} key={reserMisson.mission_id} />)}
-    </>
+    <div className="table-wrapper">
+      <table className="table table-striped mission-section">
+        <thead>
+          <tr>
+            <th>Mission</th>
+          </tr>
+        </thead>
+        {reservedMissions && reservedMissions.map((reserMisson) => <MissionCardProfile reserMisson={reserMisson} key={reserMisson.mission_id} />)}
+      </table>
+    </div>
   );
 };
 
