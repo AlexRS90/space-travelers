1efed38d194b5403bfa61c117bc26f9636d5c888
micheaol <micheaol80@gmail.com>
2021-12-15 18:05:06 +0100

    add style to the badges

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -9,7 +9,7 @@ const MissionCard = ({ mission, joinTheMission, leaveMission }) => (
       <td className="mission-title">{mission.mission_name}</td>
       <td className="mission-desc">{mission.description}</td>
       <td className="join-mission">
-        <span className="me-3 span-top" style={mission.reserved === true ? { backgroundColor: 'lightblue' } : { backgroundColor: 'red' }}>{mission.reserved === true ? 'Active Member' : 'NOT A MEMBER'}</span>
+        <span className="me-3 span-top" style={mission.reserved === true ? { backgroundColor: '#0033FF' } : { backgroundColor: '#282828' }}>{mission.reserved === true ? 'Active Member' : 'NOT A MEMBER'}</span>
       </td>
       <td className="member-status">
         <button type="button" className="span-down" onClick={mission.reserved === true ? leaveMission : joinTheMission}>{mission.reserved === true ? 'Leave mission' : 'Join Mission'}</button>
