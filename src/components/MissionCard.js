15aa6054456fe82bb145df1f4b00d1d09ba45506
micheaol <micheaol80@gmail.com>
2021-12-15 18:14:36 +0100

    add styling to join mission button

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -12,7 +12,7 @@ const MissionCard = ({ mission, joinTheMission, leaveMission }) => (
         <span className="me-3 span-top" style={mission.reserved === true ? { backgroundColor: '#0033FF' } : { backgroundColor: '#282828' }}>{mission.reserved === true ? 'Active Member' : 'NOT A MEMBER'}</span>
       </td>
       <td className="member-status">
-        <button type="button" className="span-down" onClick={mission.reserved === true ? leaveMission : joinTheMission}>{mission.reserved === true ? 'Leave mission' : 'Join Mission'}</button>
+        <button type="button" className="span-down" style={mission.reserved === true ? { backgroundColor: '#F8F8F8', color: 'red', border: '2px solid red' } : { backgroundColor: '#F8F8F8', color: 'black', border: '2px solid black' }} onClick={mission.reserved === true ? leaveMission : joinTheMission}>{mission.reserved === true ? 'Leave mission' : 'Join Mission'}</button>
       </td>
     </tr>
   </tbody>
